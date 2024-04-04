7e5746c7774823d9a36ba73c0c2311489f5f20aa
acspacelit <161153689+acspacelit@users.noreply.github.com>
2024-04-04 20:14:24 +0000

    srg

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -46,9 +46,12 @@ if uploaded_file is not None:
         rmse = mean_squared_error(y_test, y_pred, squared=False)
         r2 = r2_score(y_test, y_pred)
 
-        # Mostrar métricas
-        st.write(f"RMSE: {rmse}")
-        st.write(f"R^2: {r2}")
+        # Mostrar métricas con st.metric
+        st.write("El RMSE es el rango de la variación del Error de la predicción respecto a la predicción")
+        st.metric(label="RMSE", value=f"{rmse:.2f}")
+        st.write("Es el valor de R cuadrado del Modelo")
+        st.metric(label="R^2", value=f"{r2:.2f}")
+
 
         # Crea secciones en tu aplicación para las entradas del usuario
         st.header('Entradas para Predicción de Porcentaje Acumulado')
@@ -60,7 +63,21 @@ if uploaded_file is not None:
             'SubSector': ['AYS', 'TYL', 'SYE','PRO','GOB','VDU','AMB','FIN','ENE'],
             'TipodePrestamo': ['EMERGENCIA', 'PRÉSTAMO', 'PROGRAMA GLOBAL DE OBRAS MULTIPLES', 'PROYECTOS ESPECIFICOS DE INVERSION','PROGRAMA EN FUNCION DE RESULTADOS', 'PROGRAMA PARA EL FINANCIAMIENTO PROPORCIONAL DE INVERSIONES'],
             'Pais': ['ARGENTINA', 'BOLIVIA', 'BRASIL', 'PARAGUAY', 'URUGUA'],
-            'IDOperacion' :['AR019_1' ,'AR020_1', 'AR021_1', 'AR022_1', 'AR024_1', 'AR025_1', 'AR026_1', 'AR027_1', 'AR028_1', 'AR030_1', 'AR031_1', 'AR031_2', 'AR033_1', 'AR036_1', 'AR037_1', 'AR038_1', 'AR040_1', 'AR043_1', 'AR043_2', 'AR044_1', 'AR044_2', 'AR046_1', 'AR048_1', 'AR050_1', 'BO020_1', 'BO021_1', 'BO022_1', 'BO023_1', 'BO024_1', 'BO025_1', 'BO028_1', 'BO029_1', 'BO030_1', 'BO032_1', 'BO032_2', 'BR016_1', 'BR025_1', 'PY020_1', 'PY020_2', 'PY021_1', 'PY026_1', 'UR014_1', 'UR016_1', 'UR017_1', 'UR018_1', 'UR019_1', 'UR020_1', 'UR021_1', 'UR022_1', 'UR023_1', 'UR024_1']
+            'IDOperacion' :[
+                                "AR019_1", "AR022_1", "AR023_1", "AR024_1", "AR026_1", "AR027_1", "AR028_1", "AR028_2", "AR029_2", 
+                                "AR031_1", "AR031_2", "AR032_1", "AR033_1", "AR034_1", "AR038_1", "AR042_1", "AR043_1", "AR043_2", 
+                                "AR044_1", "AR044_2", "AR045_1", "AR046_1", "AR047_1", "AR048_2", "AR052_1", "AR059_1", "AR060_1", 
+                                "AR061_1", "BO020_1", "BO025_1", "BO028_1", "BO029_1", "BO036_1", "BO037_1", "BR019_1", "BR020_1", 
+                                "BR024_1", "BR030_1", "BR034_1", "BR036_1", "BR037_1", "BR038_1", "BR039_1", "BR040_1", "BR041_1", 
+                                "BR042_1", "PY016_1", "PY016_2", "PY020_1", "PY021_1", "PY022_1", "PY025_1", "PY027_1", "PY027_2", 
+                                "UR014_1", "UR016_1", "UR017_1", "UR018_1", "UR019_1", "UR020_1", "UR021_1", "UR022_1", "UR022_2", 
+                                "UR023_1", "UR023_2", "UR024_1", "UR024_2", "UR025_1", "UR025_2", "UR025_3", "UR025_4", "BR026_1", 
+                                "BR027_1", "PY026_1", "PY029_1", "AR020_1", "AR021_1", "AR025_1", "AR029_1", "AR030_1", "AR035_1", 
+                                "AR036_1", "AR037_1", "AR039_1", "AR040_1", "AR041_1", "AR042_2", "AR048_1", "AR049_1", "AR050_1", 
+                                "AR051_1", "AR053_1", "AR054_1", "AR055_1", "AR056_1", "AR057_1", "AR058_1", "BO021_1", "BO022_1", 
+                                "BO023_1", "BO024_1", "BO026_1", "BO027_1", "BO030_1", "BO032_1", "BO032_2", "BO033_1", "BO034_1", 
+                                "BO035_1", "BR016_1", "BR017_1", "BR018_1", "BR021_1", "BR022_1", "BR023_1", "BR025_1", "BR028_1", 
+                                "BR029_1", "BR031_1", "BR032_1", "BR033_1", "BR035_1", "PY020_2", "PY023_1", "PY024_1", "PY028_1"]
         }
 
         # Características categóricas: para cada una, creamos un selectbox con las opciones
